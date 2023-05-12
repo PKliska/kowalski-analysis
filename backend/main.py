cf956a31dab7cc1ed851ef74ebefc1ff97039dfb
Eugen Bosnjak <eugen.bosnjak@fer.hr>
2023-05-12 11:32:15 +0200

    Separated models into models.py

--- backend/main.py
+++ backend/main.py
@@ -1,42 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
-class SampleModel(BaseModel):
-    FamilyName: str
-    ProductName: str
-    Name: str
-    Id: int
-
-class InputConditionModel(BaseModel):
-    Parameter: str
-    Min: float
-    Typical: float
-    Max: float
-    TimeBetweenPoints: float
-    Id: int
-
-class ProjectModel(BaseModel):
-    Name: str
-    Samples: list[SampleModel]
-    InputConditions: list[InputConditionModel]
-    Id: int
-
-class TestPointModel(BaseModel):
-    Value: float
-    Unit: str | None
-
-class TestPointCollectionModel(BaseModel):
-    InputConditionId: int
-    SampleIds: list[int]
-    TestPoints: list[TestPointModel]
-    Id: int
-
-class JSONProjectModel(BaseModel):
-    Project: ProjectModel
-    TestPointCollections: list[TestPointCollectionModel]
+import models
 
 app = FastAPI()
 
 @app.post("/load-project")
-async def load(jsonProject: JSONProjectModel):
+async def load(jsonProject: models.JSONProjectModel):
     return { "project-name": jsonProject.Project.Name }
