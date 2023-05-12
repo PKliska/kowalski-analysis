b4a6239522765312b612a971adbb1a23d8687e11
Eugen Bosnjak <eugen.bosnjak@fer.hr>
2023-05-12 14:44:49 +0200

    Added code that manually joins data

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import itertools
+import pandas as pd
 
 import models
 
@@ -6,7 +8,27 @@ app = FastAPI()
 
 projects = { }
 
-@app.post("/load-project")
-async def loadProject(jsonProject: models.JSONProjectModel):
-    projects[jsonProject.Project.Id] = jsonProject
-    return { "ProjectId": jsonProject.Project.Id }
+@app.post("/{sampleId}/construct-csv")
+async def constructCSV(sampleId: int, jsonProject: models.JSONProjectModel):
+    test_point_collections = [collection
+                              for collection in jsonProject.TestPointCollections
+                              if sampleId in collection.SampleIds]
+    input_condition_ids = list(set(
+        [collection.InputConditionId
+         for collection in test_point_collections]))
+    input_condition_ids.sort()
+    table_header = []
+    possible_values = []
+    for input_condition_id in input_condition_ids:
+        for input_condition in jsonProject.Project.InputConditions:
+            if input_condition.Id == input_condition_id:
+                table_header.append(input_condition.Parameter)
+                break
+        values = []
+        for collection in test_point_collections:
+            if collection.InputConditionId == input_condition_id:
+                values.extend(collection.TestPoints)
+        possible_values.append(values)
+    
+    return [len(i) for i in possible_values]
+
