4c83cd4ef58c79d7274cbab852bbb046c1259ae4
Eugen Bosnjak <eugen.bosnjak@fer.hr>
2023-05-12 13:51:42 +0200

    Removed unnecessary endpoints

--- backend/main.py
+++ backend/main.py
@@ -10,15 +10,3 @@ projects = { }
 async def loadProject(jsonProject: models.JSONProjectModel):
     projects[jsonProject.Project.Id] = jsonProject
     return { "ProjectId": jsonProject.Project.Id }
-
-@app.get("/{id}/input-conditions")
-async def getInputConditions(id: int):
-    return { "InputConditions": projects[id].Project.InputConditions }
-
-@app.get("/{id}/samples")
-async def getSamples(id: int):
-    return { "Samples": projects[id].Project.Samples }
-
-@app.get("/{id}/test-point-collections")
-async def getTestPointCollections(id: int):
-    return { "TestPointCollections": projects[id].TestPointCollections }
