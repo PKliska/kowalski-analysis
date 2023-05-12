78f5a791dabee518c058089932ee29bebe41cf32
Nitko Nitkić <nitko12@hotmail.com>
2023-05-12 13:50:47 +0200

    Add personalised page

---
+++ frontend/src/_mock/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'David',
+  email: 'demo@minimals.cc',
+  photoURL: '/assets/images/avatars/david.png',
+};
+
+export default account;
